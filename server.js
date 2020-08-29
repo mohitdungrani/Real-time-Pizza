32861f4dcb80d98404b9bdd93010bbee73edae39
Mohit Dungrani <mohitdungarani@gmail.com>
2020-08-29 22:40:06 +0530

    add cart, auth

--- server.js
+++ server.js
@@ -6,15 +6,28 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 var PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-    res.render('home')
-})
-
 app.use(express.static('public'))
 app.use(expressLayout)
 
 app.set('views', path.join(__dirname, 'resources/views'))
 app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+app.get("/cart", (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get("/login", (req, res) => {
+    res.render('auth/login')
+})
+
+app.get("/register", (req, res) => {
+    res.render('auth/register')
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`)
